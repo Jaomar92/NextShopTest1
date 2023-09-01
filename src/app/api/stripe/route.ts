3ab9fd2c2e8eac04ad25da960e0d9b548cf70de8
Jaomar92 <65635396+Jaomar92@users.noreply.github.com>
2023-09-02 00:33:51 +0700

    small fixes

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 import { createOrder, updateGameQuantity } from "@/libs/api";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2022-11-15",
+  apiVersion: "2023-08-16",
 });
 
 export async function POST(req: Request, res: Response) {
